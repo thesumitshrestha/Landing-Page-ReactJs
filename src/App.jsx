09cae9a1114d396337431f7034e15f7259e448d0
Sumit Shrestha <summitshrestha8@gmail.com>
2022-08-04 12:09:28 -0500

    Added Favicon

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ function App() {
           <h2 className="text-2xl md:text-4xl lg:text-6xl uppercase">
               Welcome to  
           </h2>
-          <h1 className="text-3xl md:text-6xl lg:text-8xl font-black uppercase mb-8">
-            The coding club
+          <h1 className="text-3xl md:text-6xl lg:text-8xl font-black uppercase mb-8 mt-4">
+            Sumit Subscribers
           </h1>
           <div className="text-lg md:text-2xl lg:text-3xl py-2 px-4 md:py-4 md:px-10 lg:py-6 lg:px-12 
                         bg-white bg-opacity-10 w-fit mx-auto mb-8 rounded-full">
-              942,243 members
+            1,243 members
           </div>
 
           <form 
